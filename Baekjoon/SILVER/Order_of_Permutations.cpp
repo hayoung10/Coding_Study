0d66b5a9c3eb58b0d552a708f668a527a662a94c
mhymsy <mhymsy@gmail.com>
2021-11-15 13:07:15 +0900

    Modify Order_of_Permutations.cpp

--- Baekjoon/SILVER/Order_of_Permutations.cpp
+++ Baekjoon/SILVER/Order_of_Permutations.cpp
@@ -1,28 +1,29 @@
 #include <iostream>
-#include <algorithm> // next_permuttion
 using namespace std;
 
-int N, Q, k; // Q : question number
-int arr[21];
-int ans[21];
-bool check[21];
+int N, Q; // Q : question number
+long long arr[21];
+long long ans[21];
+bool check[21] = { false, };
 
 int main()
 {
 	cin >> N;
 	arr[0] = 1;
-	for (int i = 1; i <= 20; i++)
+	for (int i = 1; i < 21; i++)
 		arr[i] = arr[i - 1] * i;
 
 	cin >> Q;
 	if (Q == 1) {
+		long long k;
 		cin >> k;
 		for (int i = 0; i < N; i++) {
 			for (int j = 1; j <= N; j++) {
 				if (check[j]) continue;
 				if (arr[N - i - 1] < k) {
 					k -= arr[N - i - 1];
-				} else {
+				}
+				else {
 					ans[i] = j;
 					check[j] = true;
 					break;
@@ -32,17 +33,18 @@ int main()
 		for (int i = 0; i < N; i++)
 			cout << ans[i] << " ";
 		cout << endl;
-	} else if (Q == 2) {
-		int cnt = 0;
+	}
+	else if (Q == 2) {
+		long long cnt = 0;
+		int cmp;
 		for (int i = 0; i < N; i++) {
-			cin >> k;
-			for (int j = 1; j < k; j++)
+			cin >> cmp;
+			for (int j = 1; j < cmp; j++)
 				if (!check[j])
 					cnt += arr[N - i - 1];
-			check[k] = true;
+			check[cmp] = true;
 		}
-		cnt++;
-		cout << cnt << endl;
+		cout << cnt + 1 << endl;
 	}
 
 	return 0;
