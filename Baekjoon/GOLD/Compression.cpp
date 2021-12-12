a503b00b453b409812b358e59e8ffb0c6a45b0ff
mhymsy <mhymsy@gmail.com>
2021-12-12 23:37:28 +0900

    Add Compression.cpp

---
+++ Baekjoon/GOLD/Compression.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+string S;
+stack<int> num;
+stack<char> op; // operator
+
+
+int main() {
+	cin >> S;
+	
+	return 0;
+}
