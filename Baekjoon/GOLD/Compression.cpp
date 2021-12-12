30331cdaccbbe63982540fa5c11214137d5bcbda
mhymsy <mhymsy@gmail.com>
2021-12-12 23:49:03 +0900

    Modify and reload Compression.cpp

--- Baekjoon/GOLD/Compression.cpp
+++ Baekjoon/GOLD/Compression.cpp
@@ -1,15 +1,36 @@
 #include <iostream>
-#include <string>
-#include <stack>
 using namespace std;
 
 string S;
-stack<int> num;
-stack<char> op; // operator
+bool visited[51] = { 0, };
 
+int func1(int index) {
+	int cnt = 0;
+
+	for (int i = index; i < S.size(); i++) {
+		if (visited[i]) continue;
+
+		visited[i] = true;
+		if (S[i] == '(') {
+			int num = S[i - 1] - '0';
+			cnt--;
+			cnt += num * func1(i + 1);
+		}
+		else if (S[i] == ')') {
+			return cnt;
+		}
+		else {
+			cnt++;
+		}
+	}
+
+	return cnt;
+}
 
 int main() {
 	cin >> S;
-	
+
+	cout << func1(0) << endl;
+
 	return 0;
 }
